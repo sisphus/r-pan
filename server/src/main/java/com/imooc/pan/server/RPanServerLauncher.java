d9ca5a2d03bf995bf7f675ca438ce5641d1f9117
Daemon Wei <daemonwei123@gmail.com>
2024-04-03 13:06:34 -0700

    swagger2

--- server/src/main/java/com/imooc/pan/server/RPanServerLauncher.java
+++ server/src/main/java/com/imooc/pan/server/RPanServerLauncher.java
@@ -2,6 +2,7 @@ package com.imooc.pan.server;
 
 import com.imooc.pan.core.constants.RPanConstants;
 import com.imooc.pan.core.response.R;
+import io.swagger.annotations.Api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication(scanBasePackages = {RPanConstants.BASE_COMPONENT_SCAN_PATH})
 @ServletComponentScan(basePackages = RPanConstants.BASE_COMPONENT_SCAN_PATH)
 @RestController
+@Api("测试接口类")
 public class RPanServerLauncher {
 
     public static void main(String[] args) {
---
+++ server/src/main/java/com/imooc/pan/server/common/StartedListener.java
@@ -0,0 +1,4 @@
+package com.imooc.pan.server.common;
+
+public class StartedListener {
+}
